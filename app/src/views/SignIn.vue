b93d6e656261a58e17db5142578708b5bcc7a10b
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2019-04-28 11:35:32 +0200

    SignIn + SignUp views

---
+++ app/src/views/SignIn.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div>
+            <form @submit.prevent="onSubmit">
+                <div>
+                    <label for="email">E-mail</label>
+                    <input
+                        type="email"
+                        id="email"
+                        v-model="email"
+                    >
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        v-model="password"
+                    >
+                </div>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        onSubmit: () => {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/views/SignUp.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div>
+            <form @submit.prevent="onSubmit">
+                <div>
+                    <label for="email">E-mail</label>
+                    <input
+                        type="email"
+                        id="email"
+                        v-model="email"
+                    >
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        v-model="password"
+                    >
+                </div>
+                <div>
+                    <label for="confirm-password">Confirm password</label>
+                    <input
+                        type="password"
+                        id="confirm-password"
+                        v-model="confirmPassword"
+                    >
+                </div>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    },
+    methods: {
+        onSubmit: () => {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
