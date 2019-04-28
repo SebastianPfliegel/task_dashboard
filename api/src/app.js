0499c9637c7ab163c21da7b78b0c208294b056fc
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2019-04-28 14:26:03 +0200

    Allow CORS

--- api/src/app.js
+++ api/src/app.js
@@ -6,18 +6,38 @@ import taskRoutes from './routes/task';
 
 const app = express();
 
+// Use morgan to log to console
 app.use(morgan('dev'));
+
+// Parse json content from body
 app.use(bodyParser.json());
 
+// Allow Cross-Origin Resource Sharing
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'POST, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// Routes
 app.use('/user', userRoutes);
 app.use('/tasks', taskRoutes);
 
+// Handling not defined routes
 app.use((req, res, next) => {
     const error = new Error('Not Found');
     error.status = 404;
     next(error);
 });
 
+// Error handling
 app.use((err, req, res, next) => {
     const status = err.status || 500;
     res
